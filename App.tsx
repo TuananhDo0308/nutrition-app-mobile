6b4e92cc536e91f15ac1fd08d2bfed86d5253f0f
Binh05 <binhdt0452@gmail.com>
2024-10-27 14:30:28 +0700

    update

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUp from './src/screens/SignUp';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import SignIn from './src/screens/SignIn';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,64 +13,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={SignUp} options={{headerShown: false}} />
+        <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}} />
+        
       </Stack.Navigator>
     </NavigationContainer>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-
-  titleContainer: {
-    width: 300,
-    marginBottom: 13,
-  },
-
-  title: {
-    fontSize: 25,
-  },
-
-  titleColor: {
-    color: '#62C998',
-    fontWeight: 'bold',
-  },
-
-  inputContainer: {
-    marginTop: 208,
-  },
-
-  icon: {
-    margin: 7,
-    marginLeft: 15,
-    marginRight: 15,
-  },
-
-  inputSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 5,
-    width: 300,
-    height: 45,
-    marginBottom: 7,
-    marginTop: 7,
-  },
-
-  textInput: {
-    flex: 1,
-  },
-  
-  titleBottom: {
-    marginBottom: 15,
-    alignItems: 'center',
-  }
-});
